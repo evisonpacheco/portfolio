99d8f80a096d5fb89410ec43f2a2f314abd1590b
Evison <evison.pacheco21@gmail.com>
2024-02-01 16:15:51 -0300

    update: Inserindo componentes angular no projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactsComponent,
+    HeaderComponent,
+    FooterComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'homepage', pathMatch: 'full'},
+      { path: 'homepage', component: HomePageComponent},
+      { path: 'about', component: AboutComponent},
+      { path: 'skills', component: SkillsComponent},
+      { path: 'projects', component: ProjectsComponent},
+      { path: 'contacts', component: ContactsComponent},
+
+    ]),
   ],
   providers: [
     provideClientHydration()
